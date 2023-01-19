658a2792fd7f5e439a015e3a08e9cc144fb477f2
AlynnaRem <97416113+AlynnaRem@users.noreply.github.com>
2023-01-18 22:09:42 -0600

    Update POPULATE_HOTEL.sql

--- sql/DML/POPULATE_HOTEL.sql
+++ sql/DML/POPULATE_HOTEL.sql
@@ -5,15 +5,15 @@
 
 /* Data aligns with locations page prototype */
 
-INSERT INTO `provisio`.`HOTEL` ('Hotel_Code', 'Name', 'Address', 'City', 'State', 'Zip', 'Phone_Number') VALUES ('1', 'Colorado', '775 Carriage Dr.', 'Littleton', 'CO', '80123', '(761) 241-0618');
-INSERT INTO `provisio`.`HOTEL` ('Hotel_Code', 'Name', 'Address', 'City', 'State', 'Zip', 'Phone_Number') VALUES ('2', 'New Jersey', '748 Lancaster Drive', 'Maplewood', 'NJ', '07040', '(905) 787-7266');
-INSERT INTO `provisio`.`HOTEL` ('Hotel_Code', 'Name', 'Address', 'City', 'State', 'Zip', 'Phone_Number') VALUES ('3', 'New York', '7717 Iroquois Ave.', 'Wappingers Falls', 'NY', '12590', '(251) 619-2773');
-INSERT INTO `provisio`.`HOTEL` ('Hotel_Code', 'Name', 'Address', 'City', 'State', 'Zip', 'Phone_Number') VALUES ('4', 'Georgia', '7900 West New Saddle Street', 'Chatsworth', 'GA', '30705', '(438) 984-7739');
-INSERT INTO `provisio`.`HOTEL` ('Hotel_Code', 'Name', 'Address', 'City', 'State', 'Zip', 'Phone_Number') VALUES ('5', 'North Carolina', '784 York St.', 'Fuquay Varina', 'NC', '27526', '(478) 675-6563');
-INSERT INTO `provisio`.`HOTEL` ('Hotel_Code', 'Name', 'Address', 'City', 'State', 'Zip', 'Phone_Number') VALUES ('6', 'South Carolina', '8932 North Golden Star St.', 'Inman', 'SC', '29349', '(760) 215-8869');
-INSERT INTO `provisio`.`HOTEL` ('Hotel_Code', 'Name', 'Address', 'City', 'State', 'Zip', 'Phone_Number') VALUES ('7', 'Florida', '20 Kent St.', 'Sebastian', 'FL', '32958', '(325) 477-7246');
-INSERT INTO `provisio`.`HOTEL` ('Hotel_Code', 'Name', 'Address', 'City', 'State', 'Zip', 'Phone_Number') VALUES ('8', 'Ohio', '17 Oak Valley Lane', 'New Philadelphia', 'OH', '46663', '(472) 415-7671');
-INSERT INTO `provisio`.`HOTEL` ('Hotel_Code', 'Name', 'Address', 'City', 'State', 'Zip', 'Phone_Number') VALUES ('9', 'Virginia', '7727 Hillcrest St.', 'Herndon', 'VA', '20170', '(966) 476-1337');
-INSERT INTO `provisio`.`HOTEL` ('Hotel_Code', 'Name', 'Address', 'City', 'State', 'Zip', 'Phone_Number') VALUES ('10', 'California', '805 South Summerhouse St.', 'Atwater', 'CA', '95301', '(433) 831-5813');
-INSERT INTO `provisio`.`HOTEL` ('Hotel_Code', 'Name', 'Address', 'City', 'State', 'Zip', 'Phone_Number') VALUES ('11', 'Maryland', '9384 Blackburn Street', 'Waldorf', 'MD', '20601', '(787) 928-7888');
-INSERT INTO `provisio`.`HOTEL` ('Hotel_Code', 'Name', 'Address', 'City', 'State', 'Zip', 'Phone_Number') VALUES ('12', 'Minnesota', '50 New Street', 'Prior Lake', 'MN', '55372', '(217) 950-2350');
+INSERT INTO provisio.HOTEL (Hotel_Code, Name, Address, City, State, Zip, Phone_Number) VALUES ('1', 'Colorado', '775 Carriage Dr.', 'Littleton', 'CO', '80123', '(761) 241-0618');
+INSERT INTO provisio.HOTEL (Hotel_Code, Name, Address, City, State, Zip, Phone_Number) VALUES ('2', 'New Jersey', '748 Lancaster Drive', 'Maplewood', 'NJ', '07040', '(905) 787-7266');
+INSERT INTO provisio.HOTEL (Hotel_Code, Name, Address, City, State, Zip, Phone_Number) VALUES ('3', 'New York', '7717 Iroquois Ave.', 'Wappingers Falls', 'NY', '12590', '(251) 619-2773');
+INSERT INTO provisio.HOTEL (Hotel_Code, Name, Address, City, State, Zip, Phone_Number) VALUES ('4', 'Georgia', '7900 West New Saddle Street', 'Chatsworth', 'GA', '30705', '(438) 984-7739');
+INSERT INTO provisio.HOTEL (Hotel_Code, Name, Address, City, State, Zip, Phone_Number) VALUES ('5', 'North Carolina', '784 York St.', 'Fuquay Varina', 'NC', '27526', '(478) 675-6563');
+INSERT INTO provisio.HOTEL (Hotel_Code, Name, Address, City, State, Zip, Phone_Number) VALUES ('6', 'South Carolina', '8932 North Golden Star St.', 'Inman', 'SC', '29349', '(760) 215-8869');
+INSERT INTO provisio.HOTEL (Hotel_Code, Name, Address, City, State, Zip, Phone_Number) VALUES ('7', 'Florida', '20 Kent St.', 'Sebastian', 'FL', '32958', '(325) 477-7246');
+INSERT INTO provisio.HOTEL (Hotel_Code, Name, Address, City, State, Zip, Phone_Number) VALUES ('8', 'Ohio', '17 Oak Valley Lane', 'New Philadelphia', 'OH', '46663', '(472) 415-7671');
+INSERT INTO provisio.HOTEL (Hotel_Code, Name, Address, City, State, Zip, Phone_Number) VALUES ('9', 'Virginia', '7727 Hillcrest St.', 'Herndon', 'VA', '20170', '(966) 476-1337');
+INSERT INTO provisio.HOTEL (Hotel_Code, Name, Address, City, State, Zip, Phone_Number) VALUES ('10', 'California', '805 South Summerhouse St.', 'Atwater', 'CA', '95301', '(433) 831-5813');
+INSERT INTO provisio.HOTEL (Hotel_Code, Name, Address, City, State, Zip, Phone_Number) VALUES ('11', 'Maryland', '9384 Blackburn Street', 'Waldorf', 'MD', '20601', '(787) 928-7888');
+INSERT INTO provisio.HOTEL (Hotel_Code, Name, Address, City, State, Zip, Phone_Number) VALUES ('12', 'Minnesota', '50 New Street', 'Prior Lake', 'MN', '55372', '(217) 950-2350');
