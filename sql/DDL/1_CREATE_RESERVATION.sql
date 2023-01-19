7c406be3af4f7805e04944a21336b5bf3c6b6062
Jacob Stevens <35597874+jstevens88@users.noreply.github.com>
2023-01-19 13:03:58 -0800

    Updated Script for 1_CREATE_RESERVATION

--- sql/DDL/1_CREATE_RESERVATION.sql
+++ sql/DDL/1_CREATE_RESERVATION.sql
@@ -2,3 +2,27 @@
 	Bellevue University
 	CSD-460 Capstone in Software Development
 */
+/* NEEDS ALL OTHER TABLES CREATED TO RUN */
+
+DROP TABLE IF EXISTS provisio.RESERVATION;
+
+
+CREATE TABLE provisio.RESERVATION (
+  Reservation_ID timestamp NOT NULL,
+  Customer_ID integer(10) NOT NULL,
+  Hotel_Code integer(10) NOT NULL,
+  Room_ID integer(10) NOT NULL,
+  Check_In_Date date NOT NULL,
+  Check_Out_Date date NOT NULL,
+  Number_Of_Nights integer(2) NOT NULL,
+  Number_Of_Guests integer(1) NOT NULL,
+  Amount_Due numeric(19,0) NOT NULL,
+  Loyalty_Points_Earned integer(100) NOT NULL,
+  PRIMARY KEY (Reservation_ID),
+  CONSTRAINT FK_Customer_ID FOREIGN KEY (Customer_ID)
+  REFERENCES Customer(Customer_ID),
+  CONSTRAINT FK_Hotel_Code FOREIGN KEY (Hotel_Code)
+  REFERENCES Hotel(Hotel_Code),
+  CONSTRAINT FK_Room_ID FOREIGN KEY (Room_ID)
+  REFERENCES Room(Room_ID)
+  );
