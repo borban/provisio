77f28a95ed1305b7a92cb7a4ce2db5dc964a426a
AlynnaRem <97416113+AlynnaRem@users.noreply.github.com>
2023-01-18 22:03:36 -0600

    Update 2_CREATE_HOTEL.sql

--- sql/DDL/2_CREATE_HOTEL.sql
+++ sql/DDL/2_CREATE_HOTEL.sql
@@ -4,13 +4,13 @@
 */
 
 CREATE TABLE provisio.HOTEL (
-  Hotel_Code int NOT NULL,
+  Hotel_Code int(10) NOT NULL,
   Name varchar(50) NOT NULL,
   Address varchar(50) NOT NULL,
-  City varchar (50) NOT NULL,
-  State varchar (2) NOT NULL,
-  Zip varchar (10) Not NULL,
-  Phone_Number varchar (20),
+  City varchar(50) NOT NULL,
+  State varchar(2) NOT NULL,
+  Zip varchar(10) Not NULL,
+  Phone_Number varchar(20) Not NULL,
   PRIMARY KEY (Hotel_Code),
   KEY Hotel_Code_key (Hotel_Code)
   );
