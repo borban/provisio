3f3104b3468bed273104e8ae84a20ad119844010
AlynnaRem <97416113+AlynnaRem@users.noreply.github.com>
2023-01-18 21:03:54 -0600

    Update 2_CREATE_HOTEL.sql

--- sql/DDL/2_CREATE_HOTEL.sql
+++ sql/DDL/2_CREATE_HOTEL.sql
@@ -2,3 +2,16 @@
 	Bellevue University
 	CSD-460 Capstone in Software Development
 */
+
+CREATE TABLE 'provisio'.'HOTEL' (
+  'Hotel_Code' int (10) NOT NULL,
+  'Name' varchar(50) NOT NULL,
+  'Address' varchar(50) NOT NULL,
+  'City' varchar (50) NOT NULL,
+  'State' varchar (2) NOT NULL,
+  'Zip' varchar (10) Not NULL,
+  'Phone_Number' varchar (20),
+  PRIMARY KEY ('Hotel_Code'),
+  KEY 'Hotel_Code_key' ('Hotel_Code')
+  );
+ 
