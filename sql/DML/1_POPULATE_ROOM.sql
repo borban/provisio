dd45092b647995eb2f981fb6da054f482c6d6c9a
borban <borban@@users.noreply.github.com>
2023-01-21 09:23:47 -0600

    Added populate room - some hotels have more than 1 room

---
+++ sql/DML/1_POPULATE_ROOM.sql
@@ -0,0 +1,21 @@
+/*  Team 3: Alynna Rem, Jake Stevens, Mya Nanmoo, Benjamin Orban
+	Bellevue University
+	CSD-460 Capstone in Software Development
+*/
+
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('1','Double Full Beds','12');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('2', 'Queen Suite','11');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('3', 'Double Queen Beds','10');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('4','King Suite', '9');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('5','Double Full Beds','8');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('6','Queen Suite', '7');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('7','Double Queen Beds','6');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('8','King Suite', '5');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('9','Double Full Beds','4');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('10','Queen Suite', '3');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('11','Double Queen Beds','2');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('12','King Suite','1');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('13','Double Queen Beds','1');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('14','Double Queen Beds','3');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('15','Double Queen Beds','4');
+INSERT INTO provisio.ROOM (Room_Id, Room_Size, Hotel_Code) VALUES ('16','Double Queen Beds','5');
