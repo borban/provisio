e60c8ed090920395f20f4d12390899fd7e2c8840
borban <borban@@users.noreply.github.com>
2023-01-21 09:56:23 -0600

    Reordered and added some more test data

---
+++ sql/DML/2_POPULATE_CUSTOMER.sql
@@ -0,0 +1,25 @@
+/*  Team 3: Alynna Rem, Jake Stevens, Mya Nanmoo, Benjamin Orban
+	Bellevue University
+	CSD-460 Capstone in Software Development
+*/
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (1,'Eamon', 'Shoulders', 'eshoulders0@disqus.com', 'Gal0PoWcThai', 256,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (2, 'Reamonn', 'Kyberd', 'rkyberd1@home.pl', '7Ycvd6u42B3a', 354,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (3,'Law', 'Cluckie', 'lcluckie2@addthis.com', '6jqaJQuH', 359,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (4,'Brooks', 'Hanson', 'bhanson3@reference.com', '7P9xzIaa', 423,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (5,'Daphne', 'Rove', 'drove4@nih.gov', 'qrkycfL', 435,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (6,'Brock', 'Twohig', 'btwohig5@networksolutions.com', 'v76Hm7YKsRZy', 461,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (7,'Erasmus', 'Gillopp', 'egillopp6@cpanel.net', 'YpSuSU', 250,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (8,'Wait', 'Stroud', 'wstroud7@sourceforge.net', '2r4JPSp', 416,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (9,'Eberhard', 'MacGill', 'emacgill8@ow.ly', 'dMvWZMVKs', 480,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (10,'Hadlee', 'Vogel', 'hvogel9@phpbb.com', 'NTfsv9TmCn', 293,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (11,'Wendall', 'Featherstonhaugh', 'wfeatherstonhaugha@archive.org', 'HHyfTI', 474,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (12,'Kay', 'Aberkirdo', 'kaberkirdob@weebly.com', 'epxlbBVp9AQ9', 314,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (13,'Wileen', 'Sully', 'wsullyc@yale.edu', 'ZjCjGTnU', 304,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (14,'Grove', 'Gemnett', 'ggemnettd@cnbc.com', 'VQLHJ4A9pJ', 298,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (15,'Darby', 'Eamer', 'deamere@arstechnica.com', 'ZEBdWVL', 247,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (16,'Mellisa', 'Pichan', 'mpichanf@si.edu', 'zE3KJR3', 269,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (17,'Florri', 'Timblett', 'ftimblettg@youtube.com', 'msuhqrq', 286,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (18,'Charissa', 'Peterffy', 'cpeterffyh@yellowpages.com', 'FbefxH', 443,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (19,'Hazel', 'McGowran', 'hmcgowrani@booking.com', '10hJzbv', 307,'Active');
+insert into CUSTOMER (Customer_Id, First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) values (20,'Arliene', 'Pablos', 'apablosj@slate.com', 'aTwcT5GbJ', 366,'Active');
+
