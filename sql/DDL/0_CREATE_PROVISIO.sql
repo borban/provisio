c3d330749395444b9a0f0306f86db7849e106529
borban <borban@@users.noreply.github.com>
2023-01-21 06:49:59 -0600

    Adding DB creation, user creation, and granting prvileges

---
+++ sql/DDL/0_CREATE_PROVISIO.sql
@@ -0,0 +1,11 @@
+/*  Team 3: Alynna Rem, Jake Stevens, Mya Nanmoo, Benjamin Orban
+	Bellevue University
+	CSD-460 Capstone in Software Development
+*/
+DROP DATABASE IF EXISTS `provisio`;
+CREATE DATABASE `provisio`;
+
+CREATE USER 'provisio'@'localhost' IDENTIFIED BY 'provisio';
+GRANT ALL PRIVILEGES ON provisio.* TO 'provisio'@'localhost';
+
+FLUSH PRIVILEGES;
