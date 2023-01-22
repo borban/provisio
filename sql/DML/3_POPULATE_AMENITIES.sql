1a2f7026948395b8c5b52fbfb574406302706551
AlynnaRem <97416113+AlynnaRem@users.noreply.github.com>
2023-01-21 18:42:16 -0600

    Update and rename 3_POPULATE_AMENTITIES.sql to 3_POPULATE_AMENITIES.sql
    Corrected spelling for amenity.

---
+++ sql/DML/3_POPULATE_AMENITIES.sql
@@ -0,0 +1,7 @@
+/*  Team 3: Alynna Rem, Jake Stevens, Mya Nanmoo, Benjamin Orban
+	Bellevue University
+	CSD-460 Capstone in Software Development
+*/
+INSERT INTO provisio.AMENITIES (Amenity_Id, Description, Price) VALUES (1,'Wi-Fi',12.99);
+INSERT INTO provisio.AMENITIES (Amenity_Id, Description, Price) VALUES (2, 'Breakfast',8.99);
+INSERT INTO provisio.AMENITIES (Amenity_Id, Description, Price) VALUES (3, 'Parking',19.99);
--- sql/DML/3_POPULATE_AMENTITIES.sql
+++
@@ -1,7 +0,0 @@
-/*  Team 3: Alynna Rem, Jake Stevens, Mya Nanmoo, Benjamin Orban
-	Bellevue University
-	CSD-460 Capstone in Software Development
-*/
-INSERT INTO provisio.AMENTITIES (Amentity_Id, Description, Price) VALUES (1,'Wi-Fi',12.99);
-INSERT INTO provisio.AMENTITIES (Amentity_Id, Description, Price) VALUES (2, 'Breakfast',8.99);
-INSERT INTO provisio.AMENTITIES (Amentity_Id, Description, Price) VALUES (3, 'Parking',19.99);
