6437d7cd200062445e6bb14a3a37e991f3e713ec
AlynnaRem <97416113+AlynnaRem@users.noreply.github.com>
2023-01-21 21:27:41 -0600

    Update 6_CREATE_RESERVATION_AMENITIES.sql

--- sql/DDL/6_CREATE_RESERVATION_AMENITIES.sql
+++ sql/DDL/6_CREATE_RESERVATION_AMENITIES.sql
@@ -11,7 +11,7 @@ CREATE TABLE provisio.RESERVATION_AMENITIES (
   Reservation_Id integer(10) NOT NULL,
   Quantity integer(1),
   PRIMARY KEY (Res_Amenity_Id),
-  KEY Amenity_Id_Key (Res_Amenity_Id),
+  KEY Res_Amenity_Id_Key (Res_Amenity_Id),
   CONSTRAINT FK_Amenity_Id FOREIGN KEY (Amenity_Id) REFERENCES Amenities(Amenity_Id),
   CONSTRAINT FK_Reservation_Id FOREIGN KEY (Reservation_Id) REFERENCES Reservation(Reservation_Id)
   );
