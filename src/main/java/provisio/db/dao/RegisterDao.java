4e03bc1c634cbda6c0e0b293a45ef327f3f4db1d
borban <borban@@users.noreply.github.com>
2023-01-27 15:43:04 -0600

    Added data access object to split DB and controller

---
+++ src/main/java/provisio/db/dao/RegisterDao.java
@@ -0,0 +1,52 @@
+package provisio.db.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import provisio.db.model.Customer;
+
+public class RegisterDao {
+	static final String DB_URL = "jdbc:mysql://localhost:3306/provisio";
+	static final String USER = "provisio";
+	static final String PASS = "provisio";
+	
+	public boolean addCustomer(Customer customer) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		int i = 0;
+		if (customer.getEmail() != null) {
+			PreparedStatement ps = null;
+			Connection conn = null;
+			try {
+				conn = DriverManager.getConnection(DB_URL, USER, PASS);
+				if (conn != null) {
+					String sql = "INSERT INTO CUSTOMER(First_Name, Last_Name, Email, Password, Total_Loyalty_Points, Member_Status) VALUES(?,?,?,?,?,?)";
+					ps = conn.prepareStatement(sql);
+					ps.setString(1, customer.getFirstName());
+					ps.setString(2, customer.getLastName());
+					ps.setString(3, customer.getEmail());
+					ps.setString(4, customer.getPassword());
+					ps.setInt(5, 0);
+					ps.setString(6, "Active");
+					i = ps.executeUpdate();
+					System.out.println("Data Added Successfully");
+				}
+			}catch (Exception e) {
+				System.out.println(e);
+				} finally {
+					try {
+				conn.close();
+				ps.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+					}
+			}
+		}
+		return i > 0;
+			
+	}
+}
