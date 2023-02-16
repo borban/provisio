dbae1ed7d9bcefcacd9f5626ea7af8f83af306eb
Mya-Nanmoo <97609525+Mya-Nanmoo@users.noreply.github.com>
2023-02-16 14:10:17 -0600

    updated for Loyalty tracking page point status

--- src/main/java/provisio/ui/beans/LoginBean.java
+++ src/main/java/provisio/ui/beans/LoginBean.java
@@ -1,11 +1,15 @@
 package provisio.ui.beans;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 import provisio.db.dao.LoginDao;
+import provisio.db.dao.ReservationLookupDao;
 import provisio.db.model.Customer;
+import provisio.db.model.Reservation;
 import provisio.util.HashClass;
 
 /*Methods used to get get customer email and password from the login form, 
@@ -16,14 +20,14 @@ import provisio.util.HashClass;
 
 @ManagedBean(name = "loginBean", eager = true)
 @SessionScoped
-public class LoginBean {
+public class LoginBean{
 	public Customer customer = new Customer();
 	private LoginDao loginDao = new LoginDao();
 
 	public String login() {
 		Customer dbCustomerLogin = loginDao.getCustomerLogin(customer.getEmail());
 		if (isValidMember(dbCustomerLogin)) {
-			customer.setFirstName(dbCustomerLogin.getFirstName());
+                        customer.setFirstName(dbCustomerLogin.getFirstName());
 			customer.setLastName(dbCustomerLogin.getLastName());
 			customer.setCustomerId(dbCustomerLogin.getCustomerId());
 			customer.setEmail(dbCustomerLogin.getEmail());
@@ -34,7 +38,8 @@ public class LoginBean {
 	}
 
 	public void logout() {
-		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		customer = new Customer();
+                FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
 		FacesContext.getCurrentInstance().getApplication().getNavigationHandler()
 				.handleNavigation(FacesContext.getCurrentInstance(), null, "/login.xhtml");
 	}
@@ -55,5 +60,41 @@ public class LoginBean {
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
+        
+        public List<Reservation> customerReservations(){
+            ReservationLookupDao rd = new ReservationLookupDao();
+            List<Reservation> reservations = rd.lookupReservationsByCustomerId(customer.getCustomerId());
+            return reservations != null ? reservations : new ArrayList<Reservation>();
+        }
+        
+        public String getMemberStatus(){
+            int totalCustomerDays = 0;
+            String status;
+            
+            if(customerReservations() != null){
+                for(Reservation res: customerReservations()){
+                    totalCustomerDays += res.getNumberOfNights();
+                }
+            }
+            
+            
+            // calculate the customer status
+            if(totalCustomerDays <= 3){
+                status = "Brown";
+            }
+
+            else if(totalCustomerDays <= 7){
+                status = "Gold";
+            }
+
+            else if(totalCustomerDays <= 15){
+                status = "Platinum";
+            }
+            else{
+                status = "Diamond";
+            }
+            
+            return status;
+        }
 
 }
